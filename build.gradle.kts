68b06d9bebd9623c389cb8686855f50a729d5c87
huutq98 <huutq98@gmail.com>
2023-02-16 00:12:38 +0700

    Upgrade gradle version for Giraffe Android Studio version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.0.0-alpha06").apply(false)
-    id("com.android.library").version("8.0.0-alpha06").apply(false)
+    id("com.android.application").version("8.1.0-alpha04").apply(false)
+    id("com.android.library").version("8.1.0-alpha04").apply(false)
     kotlin("android") version libs.versions.kotlin.get() apply false
     kotlin("multiplatform") version libs.versions.kotlin.get() apply false
     kotlin("plugin.serialization") version libs.versions.kotlin.get() apply false
