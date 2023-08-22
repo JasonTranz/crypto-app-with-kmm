190bb121742e69387cf6b243fdbbb166028c16cb
jasontran <huutq98@gmail.com>
2023-08-23 00:17:30 +0700

    Some UI improvements

--- androidApp/src/main/java/com/cryptochain/mota/android/feature/search/SearchScreen.kt
+++ androidApp/src/main/java/com/cryptochain/mota/android/feature/search/SearchScreen.kt
@@ -2,12 +2,19 @@ package com.cryptochain.mota.android.feature.search
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun SearchScreen() {
-    Column(verticalArrangement = Arrangement.Center) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(text = "Search")
     }
 }
